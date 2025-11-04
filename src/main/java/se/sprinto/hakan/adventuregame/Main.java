efdca2ec0c993219b16963330d65c6e8689ce7b9
philipwmitilinos-max <philipwmitilinos@gmail.com>
2025-11-04 12:41:02 +0100

    Testar att lägga upp project till Github. Försöker att lägga till scr till Github.
    Version 1.1 - Lägg till AppInfo.class, config.properties-fil och instantiate i Main.

--- src/main/java/se/sprinto/hakan/adventuregame/Main.java
+++ src/main/java/se/sprinto/hakan/adventuregame/Main.java
@@ -6,6 +6,7 @@ import se.sprinto.hakan.adventuregame.model.Player;
 import se.sprinto.hakan.adventuregame.model.StartRoom;
 import se.sprinto.hakan.adventuregame.model.Statistics;
 import se.sprinto.hakan.adventuregame.service.StatisticsService;
+import se.sprinto.hakan.adventuregame.view.AppInfo;
 import se.sprinto.hakan.adventuregame.view.ScannerUI;
 import se.sprinto.hakan.adventuregame.view.UI;
 
@@ -14,7 +15,11 @@ public class Main {
     public static void main(String[] args) {
         UI ui = new ScannerUI();
         ui.showMessage("Välkommen till Äventyrsspelet!");
-        ui.showMessage("Version 1.0 av Håkan Gleissman");
+//        ui.showMessage("Version 1.0 av Håkan Gleissman");
+        AppInfo appInfo = AppInfo.getInstance();                // Hämtar AppInfo
+        String version = appInfo.getProperty("app.version");    // Hämtar versionen
+        String author = appInfo.getProperty("app.author");      // Hämtar författaren
+        ui.showMessage("Version " + version + " av " + author); // Skriver ut de nya versionen och författaren
         String name = ui.getInput("Ange ditt namn:");
         Player player = new Player(name, 100, 0, 10);
 
